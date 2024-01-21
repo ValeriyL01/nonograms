4d30e6745451618bea7122577a8024164952d8f2
ValeriyL01 <lastovka.valeriy.00@mail.ru>
2024-01-21 20:07:23 +0300

    feat: add winning algorithm and select Form Images

--- nonograms/src/components/createSettingsButton.js
+++ nonograms/src/components/createSettingsButton.js
@@ -4,27 +4,28 @@ const createElement = (tag, className) => {
   return element;
 };
 let selectFormLevels;
+let selectFormImages;
+let resetGame;
 const createSettingsButtons = () => {
   const settings = createElement('div', 'settings');
-  const resetGame = createElement('button', 'settings__reset');
-
+  resetGame = createElement('button', 'settings__reset');
   const solution = createElement('button', 'settings__solution');
   const continueLastGame = createElement('button', 'settings__continue-last-game');
   const randomGame = createElement('button', 'settings__random-game');
   const themeGame = createElement('button', 'settings__theme');
+
   resetGame.innerText = 'reset';
   solution.innerText = 'solution';
   continueLastGame.innerText = 'continue last game';
   randomGame.innerText = 'random game';
   themeGame.innerText = 'theme';
-
+  // выбор размера игрового поля
   const levelsForm = createElement('form', 'settings__form');
   const labelFormLevels = createElement('label', 'settings__form-label');
   selectFormLevels = createElement('select', 'settings__form-select');
   const optionFormLevels1 = createElement('option', 'settings__form-option');
   const optionFormLevels2 = createElement('option', 'settings__form-option');
   const optionFormLevels3 = createElement('option', 'settings__form-option');
-
   labelFormLevels.setAttribute('for', 'levels');
   selectFormLevels.setAttribute('name', 'levels');
   labelFormLevels.innerText = 'levels';
@@ -35,9 +36,41 @@ const createSettingsButtons = () => {
   optionFormLevels2.innerText = '10x10';
   optionFormLevels3.value = '15';
   optionFormLevels3.innerText = '15x15';
-  levelsForm.append(labelFormLevels, selectFormLevels);
   selectFormLevels.append(optionFormLevels1, optionFormLevels2, optionFormLevels3);
+  //--
+  // выбор картинки для игры
+  const labelFormImages = createElement('label', 'settings__form-label');
+  selectFormImages = createElement('select', 'settings__form-select');
+  let optionFormImage;
+  labelFormImages.setAttribute('for', 'images');
+  selectFormImages.setAttribute('name', 'images');
+  labelFormImages.innerText = 'templates';
+  selectFormImages.value = '0';
+  let countValue = 0;
+  const nameImages = [
+    'snake(5x5)',
+    'cross(5x5)',
+    'sharp(5x5)',
+    'shess(5x5)',
+    'snowflake(5x5)',
+    'home(10x10)',
+    'ggg',
+    'gggg',
+    'ggggg',
+    'jj',
+  ];
+  for (let i = 0; i <= 9; i += 1) {
+    optionFormImage = createElement('option', 'settings__form-option');
+
+    optionFormImage.value = countValue;
+    countValue += 1;
+    optionFormImage.innerText = nameImages[i];
+    selectFormImages.append(optionFormImage);
+  }
+  //--
+
+  levelsForm.append(labelFormLevels, selectFormLevels, labelFormImages, selectFormImages);
   settings.append(resetGame, solution, continueLastGame, randomGame, themeGame, levelsForm);
   return settings;
 };
-export {createSettingsButtons, selectFormLevels};
+export {createSettingsButtons, selectFormLevels, selectFormImages, resetGame};
--- nonograms/src/index.js
+++ nonograms/src/index.js
@@ -1,10 +1,9 @@
 import './style.css';
 import './index.html';
-import {createSettingsButtons, selectFormLevels} from './components/createSettingsButton';
+import {createSettingsButtons, selectFormLevels, selectFormImages, resetGame} from './components/createSettingsButton';
 
 import data from './components/matrix';
 
-console.log(data[0].matrix);
 const createElement = (tag, className) => {
   const element = document.createElement(tag);
   element.classList.add(className);
@@ -20,11 +19,14 @@ const playingFieldWrapper = createElement('div', 'playing-field-wrapper');
 const playingFieldLeftWrapper = createElement('div', 'playing-field-left-wrapper');
 gameContainer.append(playingField, createSettingsButtons());
 playingField.append(topCluesWrapper, playingFieldLeftCluesWrapper);
+// const correctСlickСounter = 0;
+let selectValueLevels = selectFormLevels.value;
+const selectValueImages = selectFormImages.value;
 
 const createTopClues = (dataTop) => {
   for (let i = 0; i < 15; i += 1) {
     const cellTop = createElement('div', 'cell');
-    cellTop.textContent = dataTop[i];
+    cellTop.textContent = dataTop[0][1].topClues[i];
     topCluesWrapper.append(cellTop);
   }
 };
@@ -34,35 +36,112 @@ const createLeftClues = (dataLeft) => {
     const cellLeft = createElement('div', 'cell-left');
     playingFieldLeftWrapper.append(cellLeft);
 
-    cellLeft.textContent = dataLeft[i];
+    cellLeft.textContent = dataLeft[0][1].leftClues[i];
   }
 };
-createTopClues(data[0][1].topClues);
-createLeftClues(data[0][1].leftClues);
 
-const game = (matrix, level) => {
+// const arrCell = [];
+// const resetGames = (level) => {
+//   let cycleLength = 25;
+//   if (level === 5) {
+//     cycleLength = 5 * 5;
+//   } else if (level === 10) {
+//     cycleLength = 10 * 10;
+//   } else if (level === 15) {
+//     cycleLength = 15 * 15;
+//   }
+
+//   for (let i = 0; i < cycleLength; i += 1) {
+//     arrCell[i].classList.remove('cell--true');
+//     arrCell[i].classList.remove('cell--activ');
+//   }
+// };
+
+const game = (dataMatrix, level) => {
+  const arrayFilledCells = [];
+  const arrayGuessedCells = [];
+  const arrayEmptyCells = [];
+  const matrixImage = dataMatrix[0][1];
   for (let i = 0; i < level; i += 1) {
     for (let j = 0; j < level; j += 1) {
       const cell = createElement('div', 'cell');
-
+      //  arrCell.push(cell);
       cell.textContent = '';
       playingFieldWrapper.append(cell);
-      if (matrix[i][j] === 1) {
-        cell.classList.add('cell-black');
+      if (matrixImage.matrix[i][j] === 1) {
+        arrayFilledCells.push(1);
+        // cell.classList.add('cell--activ');
       }
+      let isClick = false;
+      let isClickEmptyCells = false;
       document.addEventListener('click', (event) => {
         if (event.target === cell) {
-          cell.classList.add('cell-black');
+          cell.classList.toggle('cell--activ');
+          // ---  алгоритм победы
+          if (matrixImage.matrix[i][j] === 1) {
+            if (!isClick) {
+              arrayGuessedCells.push(1);
+              isClick = true;
+            } else {
+              arrayGuessedCells.pop(1);
+              isClick = false;
+            }
+          }
+          if (matrixImage.matrix[i][j] === 0) {
+            if (!isClickEmptyCells) {
+              arrayEmptyCells.push(1);
+              isClickEmptyCells = true;
+            } else {
+              arrayEmptyCells.pop(1);
+              isClickEmptyCells = false;
+            }
+          }
+        }
+        if (arrayFilledCells.length === arrayGuessedCells.length && arrayEmptyCells.length === 0) {
+          console.log('ура');
+        }
+        //---
+      });
+
+      // клик правой кнопкой мыши
+      let isRightClick = false;
+      cell.addEventListener('contextmenu', (event) => {
+        event.preventDefault();
+        if (!isRightClick) {
+          cell.textContent = 'X';
+          isRightClick = true;
+        } else {
+          cell.textContent = '';
+          isRightClick = false;
         }
       });
+      //--
     }
   }
+  createTopClues(data);
+  createLeftClues(data);
 };
 
-let selectValue = selectFormLevels.value;
-game(data[0][1].matrix, selectValue);
+game(data, selectValueLevels);
 playingFieldLeftCluesWrapper.append(playingFieldLeftWrapper, playingFieldWrapper);
+
+// --выбор уровня игры
 selectFormLevels.addEventListener('change', () => {
-  selectValue = selectFormLevels.value;
-  console.log(selectValue);
+  selectValueLevels = selectFormLevels.value;
+
+  playingFieldWrapper.classList.add('playing-field-wrapper10');
+  topCluesWrapper.classList.add('top-clues-wrapper10');
+  playingFieldLeftWrapper.classList.add('playing-field-left-wrapper10');
 });
+//--
+
+// -- выбор картинки для игры
+// selectFormImages.addEventListener('change', () => {
+//   selectValueImages = selectFormImages.value;
+//   console.log(selectValueImages);
+// });
+//--
+
+// resetGame.addEventListener('click', () => {
+//   resetGames();
+// });
